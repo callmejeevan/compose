7d0f9490aa4db229b57aba37ca310439e63d8bdf
Jeevan <jee00723@gmail.com>
2021-05-21 17:57:43 +0530

    Just checking the commit flow

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
-                    Greeting(name = "Jeevan")
                 }
             }
         }
