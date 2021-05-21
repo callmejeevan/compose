64fd686802a41499b215b8d1f811a183240dad24
Jeevan <jee00723@gmail.com>
2021-05-21 17:47:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose.ui.theme.ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                    Greeting(name = "Jeevan")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTheme {
+        Greeting("Android")
+        Greeting("Jeevan")
+    }
+}
